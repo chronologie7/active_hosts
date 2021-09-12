f889824f9ec2c64c06021fb687abd405da2d98be
Alfonso Farías <77316261+chronologie7@users.noreply.github.com>
2021-09-11 23:12:48 -0300

    add file creation, print host and ms waiting
    file creation with hosts found, print host found and increased time waiting for responde.

--- hosts_list.py
+++ hosts_list.py
@@ -3,10 +3,18 @@
 import os
 
 activeHosts = []
+hostCount = 0
 
 for x in range(1,255):
-    result = os.system("ping -n 1 -w 500 " + "192.168.1." + str(x))
+    result = os.system("ping -n 1 -w 1250 " + "192.168.1." + str(x))
     if result == 0:
         activeHosts.append("192.168.1." + str(x))
+    os.system("cls")
 
-print(activeHosts)
+print("Creating \"active_hosts.txt\" file...")
+
+with open("active_hosts.txt", "w") as file:
+    for host in activeHosts:
+        hostCount += 1
+        print(f"host #{hostCount}: {host}")
+        file.write(f"host #{hostCount}: {host}\n")
